49e6fe9b289b093cdc1dc958ac2619dc24097278
ektedar <ektedar_alam@live.com>
2022-07-27 19:25:49 -0400

    Black Formatting and testing out github actions

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -7,25 +7,19 @@ from moto import mock_transcribe
 
 @mock_transcribe()
 class TranscibeTest(unittest.TestCase):
-
-    def aws_credentials(self):
-        """Mocked AWS Credentials for moto."""
-        os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-        os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-        os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-        os.environ['AWS_SESSION_TOKEN'] = 'testing'
-        os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
-
     def test_start_transcribe(self):
         """Unit Test to test out connection to amazon transcribe"""
-        import src.transcribe as transcribe # This import is necessary inside the function because the original function is initializing the client outside the class 
-        file_name = 'test_audio.wav'
-        transcribe_client = boto3.client('transcribe', region_name='us-east-1')
+        import src.transcribe as transcribe  # This import is necessary inside the function because the original function is initializing the client outside the class
+
+        file_name = "test_audio.wav"
+        transcribe_client = boto3.client("transcribe", region_name="us-east-1")
         transcribe_manager = transcribe.TranscibeBatch(
-            bucket_name='test_bucket',
-            file_name=file_name
+            bucket_name="test_bucket", file_name=file_name
         )
         transcribe_manager.start_transcribe()
         response = transcribe_client.list_transcription_jobs()
 
-        assert response['TranscriptionJobSummaries'][0]['TranscriptionJobName'] == file_name
+        assert (
+            response["TranscriptionJobSummaries"][0]["TranscriptionJobName"]
+            == file_name
+        )
